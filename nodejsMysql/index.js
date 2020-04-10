7adc7143a0a6cd12462843a77a734501b8191635
wangpeng <wang760635994@outlook.com>
2020-04-10 16:59:29 +0800

    2020年4月10日16:59:26

--- nodejsMysql/index.js
+++ nodejsMysql/index.js
@@ -10,7 +10,7 @@ const mysql = Mysql.createConnection({
     user: 'root', // The name of cloud database, for example, root, 云数据库用户名，如root
     password: 'root', // Password of cloud database, 云数据库密码
     database: 'list', // Name of the cloud database, 数据库名称
-    logSql:true,
+    logSql: true,
 })
 
 app.use('/public', express.static('public'));
@@ -35,7 +35,7 @@ app.get("/testadd", async (req, res) => {
     try {
         res.json({
             code: 'ok',
-            result:await mysql.table('posts').add(req.query)
+            result: await mysql.table('posts').add(req.query)
         })
     } catch (err) {
         res.json({
@@ -48,11 +48,11 @@ app.get("/testadd", async (req, res) => {
 // add
 app.get("/add", async (req, res) => {
     try {
-        const {name,age,birthday} = req.query;
-        const data = {name,age,birthday};
+        const { name, age, birthday } = req.query;
+        const data = { name, age, birthday };
         res.json({
             code: 'ok',
-            result:await mysql.table('posts').add(data)
+            result: await mysql.table('posts').add(data)
         })
     } catch (err) {
         res.json({
@@ -65,11 +65,11 @@ app.get("/add", async (req, res) => {
 // delete
 app.get("/delete", async (req, res) => {
     try {
-        const {id} = req.query;
-        const data = {id};
+        const { id } = req.query;
+        const data = { id };
         res.json({
             code: 'ok',
-            result:await mysql.table('posts').where(data).delete()
+            result: await mysql.table('posts').where(data).delete()
         })
     } catch (err) {
         res.json({
@@ -84,11 +84,11 @@ app.get("/delete", async (req, res) => {
 // update
 app.get("/update", async (req, res) => {
     try {
-        const {name,age,birthday} = req.query;
-        const data = {name,age,birthday};
+        const { name, age, birthday } = req.query;
+        const data = { name, age, birthday };
         res.json({
             code: 'ok',
-            result:await mysql.table('posts').where({id:req.query.id}).update(data)
+            result: await mysql.table('posts').where({ id: req.query.id }).update(data)
         })
     } catch (err) {
         res.json({
@@ -103,18 +103,18 @@ app.get("/update", async (req, res) => {
 // list
 app.get("/getList", async (req, res) => {
     try {
-        if(req.query.name){
+        if (req.query.name) {
             res.json({
                 code: 'ok',
                 result: await mysql.query(`SELECT * FROM posts WHERE name REGEXP '${req.query.name}'`)
             })
-        }else{
+        } else {
             res.json({
                 code: 'ok',
                 result: await mysql.table('posts').select()
             })
         }
-       
+
     } catch (err) {
         res.json({
             code: '-1',
